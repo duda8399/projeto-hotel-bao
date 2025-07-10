595f82012dc1a7d917f3f975f82e987f1b0c0576
duda8399 <mariaeduardaleal66@gmail.com>
2025-07-10 14:36:02 -0300

    add titulo da api do swagger

--- src/main/java/edu/ifmg/com/ProjetoHotelBaoApplication.java
+++ src/main/java/edu/ifmg/com/ProjetoHotelBaoApplication.java
@@ -1,8 +1,15 @@
 package edu.ifmg.com;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(
+		title = "Hotel Bão",
+		version = "1.0",
+		description = "Documentação da API do Hotel Bão"
+))
 @SpringBootApplication
 public class ProjetoHotelBaoApplication {
 
